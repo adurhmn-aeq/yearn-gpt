9f196ed389fdad52e7606ede5c91617161bcac52
Abdur <abdur@aequalisys.com>
2024-01-20 16:41:04 +0530

    Fix broken session path

--- app/ui/src/utils/agent.ts
+++ app/ui/src/utils/agent.ts
@@ -3,5 +3,5 @@ export const getSessionPath = (agentId: string) => {
 };
 
 export const getSessionLink = (agentId: string) => {
-  return `${import.meta.env.VITE_HOST_URL}${getSessionPath(agentId)}`;
+  return `${import.meta.env.VITE_HOST_URL}/#${getSessionPath(agentId)}`;
 };
