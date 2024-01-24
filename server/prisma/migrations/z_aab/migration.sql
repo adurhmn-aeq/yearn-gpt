4a5351dc9f02dd8e3e014bfc62264f4a475d79a9
Abdur <abdur@aequalisys.com>
2024-01-24 20:50:40 +0530

    Migration fix 3

---
+++ server/prisma/migrations/z_aab/migration.sql
@@ -0,0 +1,39 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "registerType" TEXT NOT NULL DEFAULT 'email-password';
+
+-- CreateTable
+CREATE TABLE "Agent" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "initMsg" TEXT NOT NULL DEFAULT 'Good day, how are you doing?',
+    "prompt" TEXT NOT NULL,
+    "temperature" DOUBLE PRECISION NOT NULL DEFAULT 0.5,
+    "model" TEXT NOT NULL DEFAULT 'gpt-3.5-turbo-dbase',
+    "user_id" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Agent_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "messages" JSON[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_id" INTEGER,
+    "agent_id" TEXT,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Agent" ADD CONSTRAINT "Agent_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("user_id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("user_id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_agent_id_fkey" FOREIGN KEY ("agent_id") REFERENCES "Agent"("id") ON DELETE SET NULL ON UPDATE CASCADE;
