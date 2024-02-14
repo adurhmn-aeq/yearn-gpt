7e61475d48f9893c9932876f58743cab589746b3
Abdur Rahman <abdur@aequalisys.com>
2024-02-14 08:50:26 +0530

    Change env name for api url in widget

--- app/widget/src/utils/getUrl.ts
+++ app/widget/src/utils/getUrl.ts
@@ -1,8 +1,8 @@
 export const getUrl = () => {
-  if (import.meta.env.VITE_WIDGET_API_URL) {
+  if (import.meta.env.VITE_API_URL_WIDGET) {
     const fullPath = window.location.href;
     const host = window.location.protocol + "//" + window.location.host;
-    const url = fullPath.replace(host, import.meta.env.VITE_WIDGET_API_URL);
+    const url = fullPath.replace(host, import.meta.env.VITE_API_URL_WIDGET);
     return url;
   }
   const url = window.location.href;
