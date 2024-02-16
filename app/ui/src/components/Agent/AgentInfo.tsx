798e25f2b46261e5d231e83dd5ce59f24a870c60
raviprasad-aeq <raviprasad@aequalisys.com>
2024-02-16 20:48:44 +0530

    user info changes.

--- app/ui/src/components/Agent/AgentInfo.tsx
+++ app/ui/src/components/Agent/AgentInfo.tsx
@@ -13,7 +13,6 @@ type Props = {
 };
 
 export default function AgentInfo({ info }: Props) {
-  console.log(info);
   return (
     <>
       {/* <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-lg">
@@ -24,35 +23,23 @@ export default function AgentInfo({ info }: Props) {
           <img src="/providers/botImg.svg" alt="" />
           <div className="flex flex-col">
             <p>{info.name}</p>
-            <p>{info.model}</p>
+            <p className="opacity-70">{info.model}</p>
           </div>
         </div>
         {/* <h1 className="text-lg font-bold ml-3">Agent Information</h1> */}
         <div className="bg-white sm:rounded-lg px-4 dark:bg-black">
-          <div className="flex flex-col gap-5">
+          <div className="flex flex-col gap-7">
             <div className="flex flex-col gap-2">
-              {/* <div className="text-sm">Agent Name:</div> */}
-              {/* <div className="ring-1 ring-gray-300 p-2 rounded-md text-sm text-gray-600"> */}
-              {/* {info.name} */}
-              {/* </div> */}
-            </div>
-            <div className="flex flex-col gap-2">
-              <div className="text-sm">Initiation Message:</div>
-              {/* <div className="ring-1 ring-gray-300 p-2 rounded-md text-sm text-gray-600"> */}
+              <div className="text-sm opacity-70">Initiation Message:</div>
               {info.initMsg}
-              {/* </div> */}
             </div>
             <div className="flex flex-col gap-2">
-              <div className="text-sm">Agent Prompt:</div>
-              {/* <div className="ring-1 ring-gray-300 p-2 rounded-md text-sm text-gray-600"> */}
+              <div className="text-sm opacity-70">Agent Prompt:</div>
               {info.prompt}
-              {/* </div> */}
             </div>
             <div className="flex flex-col gap-2">
-              <div className="text-sm">Created At:</div>
-              {/* <div className="ring-1 ring-gray-300 p-2 rounded-md text-sm text-gray-600"> */}
+              <div className="text-sm opacity-70">Created At:</div>
               {dateFormat(info.createdAt, "dddd, mmmm dS, yyyy, h:MM TT")}
-              {/* </div> */}
             </div>
           </div>
         </div>
